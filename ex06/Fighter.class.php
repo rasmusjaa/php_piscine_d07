093fc652e46dafd99d053e13098a222cc68bd463
Rasmus Jaakonmaki <rjaakonm@c2r2p2.hive.fi>
2020-07-01 13:23:58 +0300

    removed unnecessary methods and set fight to private to get fatal error if child doesn't have it

--- ex06/Fighter.class.php
+++ ex06/Fighter.class.php
@@ -2,22 +2,13 @@
 
 class Fighter {
 
-    private $recruits = [];
-    public $type;
+	public $type;
 
-    public function __construct($string) {
+	public function __construct($string) {
 		$this->type = $string;
-    }
-    
-	public function recruit($person) {
-		$this->$recruits[] = $person;
 	}
-	
-	public function fight() {
-		foreach ($this->$recruits as $recruit) {
-			if ($recruit instanceof Fighter && method_exists($recruit, 'fight'))
-				$recruit->fight();
-		}
+
+	private function fight($target) {
 	}
 }
 
